9f631677eb46d09d939caf2c0795e6b699a8cd88
haibao <1016005502@qq.com>
2020-05-22 17:18:34 +0800

    修改问题消费者和新增问题消费者合为一个类

--- straw-portal/src/main/java/cn/tedu/straw/portal/kafka/CreateOrUpdateQuestionConsumer.java
+++ straw-portal/src/main/java/cn/tedu/straw/portal/kafka/CreateOrUpdateQuestionConsumer.java
@@ -1,49 +1,57 @@
 package cn.tedu.straw.portal.kafka;
 
 import cn.tedu.straw.common.constant.KafkaTopic;
+import cn.tedu.straw.common.util.ExceptionUtil;
 import cn.tedu.straw.portal.model.EsQuestion;
+import cn.tedu.straw.portal.service.IMailService;
+import cn.tedu.straw.portal.service.IMessageService;
 import cn.tedu.straw.search.api.EsQuestionServiceApi;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import javax.annotation.Resource;
 import java.util.Optional;
 
 /**
- * @Description: 创建问题消费者
+ * @Description: 创建或更新问题消费者
  * @Author: ChenHaiBao
  * @CreateDate: 2020/5/03$ 20:15$
  * @Version: 1.0
  */
 @Component
 @Slf4j
-public class CreateQuestionConsumer {
+public class CreateOrUpdateQuestionConsumer {
 
 
     @Resource
     private EsQuestionServiceApi esQuestionServiceApi;
     private Gson gson=new Gson();
+    @Resource
+    private IMailService mailService;
 
+    @Value("${spring.mail.to.admin}")
+    private String toAdmin;
 
     /**
-     * 创建问题
+     * 创建或修改问题
      * @param record
      */
-    @KafkaListener(topics = {KafkaTopic.PORTAL_CREATE_QUESTION})
+    @KafkaListener(topics = {KafkaTopic.PORTAL_CREATE_QUESTION,KafkaTopic.PROTAL_UPDATE_QUESTION})
     public void listen(ConsumerRecord<String, String> record) {
 
         Optional<?> kafkaMessage = Optional.ofNullable(record.value());
         if (kafkaMessage.isPresent()) {
           String esQuestionStr= (String) kafkaMessage.get();
           EsQuestion esQuestion= gson.fromJson(esQuestionStr,EsQuestion.class);
-          boolean flag= esQuestionServiceApi.saveQuestion(esQuestion);
-          log.info("消费了一条创建问题的消息："+esQuestion);
-            if(!flag){
-                //TODO 通过打错误日志，或者通过kafka发送错误邮件到管理员
-                log.error("ES服务异常");
-            }
+          try{
+              boolean flag= esQuestionServiceApi.saveQuestion(esQuestion);
+          }catch (Exception e){
+              log.error(e.getMessage(),e);
+              mailService.sendSimpleMail(toAdmin,"稻草系统出错信息", ExceptionUtil.getStackTraceInfo(e));
+          }
 
         }
     }
