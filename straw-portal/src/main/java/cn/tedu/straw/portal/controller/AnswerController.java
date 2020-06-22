d4f9a20ee4b6575170058eb900036428e28de2d6
haibao <1016005502@qq.com>
2020-06-22 17:27:33 +0800

    重构代码，将采纳问题方法的代码抽取出来由answerService业务类处理。方式执行失败没有事务回滚。并用toAjax方法简化了业务方法返回的判断

--- straw-portal/src/main/java/cn/tedu/straw/portal/controller/AnswerController.java
+++ straw-portal/src/main/java/cn/tedu/straw/portal/controller/AnswerController.java
@@ -1,7 +1,6 @@
 package cn.tedu.straw.portal.controller;
 
-import cn.tedu.straw.common.StrawResult;
-import cn.tedu.straw.common.constant.RoleName;
+import cn.tedu.straw.common.R;
 import cn.tedu.straw.portal.base.BaseController;
 import cn.tedu.straw.portal.exception.BusinessException;
 import cn.tedu.straw.portal.model.Answer;
@@ -10,7 +9,6 @@ import cn.tedu.straw.portal.service.IAnswerService;
 import cn.tedu.straw.portal.service.ICommentService;
 import cn.tedu.straw.portal.service.IQuestionService;
 import cn.tedu.straw.portal.service.IRecommendQuestionService;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -37,26 +35,17 @@ public class AnswerController extends BaseController {
     private IQuestionService questionService;
     @Resource
     private IRecommendQuestionService recommendQuestionService;
-    @Resource
-    private ICommentService commentService;
+
 
     @GetMapping("/acceptAnswer")
     @ApiOperation("采纳该回答")
     public String acceptAnswer(@RequestParam("answerId")Integer answerId,@RequestParam("questionId")Integer questionId){
-        Answer answer=new Answer();
-        answer.setId(answerId);
-        answer.setAcceptStatus(true);
-        boolean a = answerService.updateById(answer);
-        //问题设置为已解决
-        Question question=new Question();
-        question.setId(questionId);
-        question.setStatus(2);
-        boolean b = questionService.updateById(question);
 
-        if(a&&b){
+        boolean isSuccess=answerService.acceptAnswer(answerId,questionId);
+        if(isSuccess){
             return "redirect:/question/detail/"+questionId;
         }
-        throw  new BusinessException("服务繁忙，请稍后再试！");
+        return "error";
     }
 
     @GetMapping("/edit/{id}")
@@ -77,29 +66,25 @@ public class AnswerController extends BaseController {
     @ApiOperation("获取答案")
     @ResponseBody
     @PreAuthorize("hasAuthority('ROLE_TEACHER')")
-    public StrawResult getById(@PathVariable("id")Integer id){
+    public R getById(@PathVariable("id")Integer id){
         Answer answer = answerService.getById(id);
         if(answer!=null){
-            return new StrawResult().success(answer);
+            return R.success(answer);
         }
 
-      return new StrawResult().failed("没有该记录");
+      return R.failed("没有该记录");
     }
 
     @PostMapping("/update")
     @ApiOperation("提交修改答案")
     @ResponseBody
     @PreAuthorize("hasAuthority('ROLE_TEACHER')")
-    public StrawResult update(@RequestParam("answerId")Integer answerId,@RequestParam("content")String content){
+    public R update(@RequestParam("answerId")Integer answerId, @RequestParam("content")String content){
         Answer answer=new Answer();
         answer.setId(answerId);
         answer.setContent(content);
-        boolean isSuccess = answerService.updateById(answer);
-        if(isSuccess){
-            return new StrawResult().success();
-        }else {
-            return new StrawResult().failed("服务器繁忙,请稍后重试！");
-        }
+       return toAjax(answerService.updateById(answer));
+
 
     }
 
@@ -107,13 +92,8 @@ public class AnswerController extends BaseController {
     @ApiOperation("删除回答")
     @PreAuthorize("hasAuthority('ROLE_TEACHER')")
     @ResponseBody
-    public StrawResult delete(@PathVariable("answerId")Integer answerId){
-      Boolean isSuccess=  answerService.deleteById(answerId);
-      if(isSuccess){
-          return new StrawResult().success();
-      }
-        return new StrawResult().failed("删除失败！");
-
+    public R delete(@PathVariable("answerId")Integer answerId){
+      return toAjax(answerService.deleteById(answerId));
     }
 
 }
