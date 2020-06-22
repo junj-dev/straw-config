9ee9e4429a475827d67d4980d23fd73cc58219e4
haibao <1016005502@qq.com>
2020-06-22 17:12:11 +0800

    重构代码，方法的返回值由原来的'Json'改为'boolean',执行成功返回true，执行失败返回false,参数或业务异常则抛出业务异常

--- straw-portal/src/main/java/cn/tedu/straw/portal/service/impl/MessageServiceImpl.java
+++ straw-portal/src/main/java/cn/tedu/straw/portal/service/impl/MessageServiceImpl.java
@@ -1,18 +1,17 @@
 package cn.tedu.straw.portal.service.impl;
 
-import cn.tedu.straw.common.StrawResult;
+import cn.tedu.straw.common.R;
 import cn.tedu.straw.common.constant.RedisKeyPrefix;
 import cn.tedu.straw.common.util.NumberUtils;
 import cn.tedu.straw.portal.config.AliyunMesageConfig;
+import cn.tedu.straw.portal.exception.BusinessException;
 import cn.tedu.straw.portal.mapper.ClassroomMapper;
 import cn.tedu.straw.portal.model.Classroom;
 import cn.tedu.straw.portal.model.User;
-import cn.tedu.straw.portal.service.IClassroomService;
 import cn.tedu.straw.portal.service.IMessageService;
 import cn.tedu.straw.portal.service.IUserService;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -25,15 +24,10 @@ import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.exceptions.ServerException;
 import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
-import org.springframework.util.StringUtils;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @Description: 短信发送业务类
@@ -57,26 +51,25 @@ public class MessageServiceImpl implements IMessageService {
     private ClassroomMapper classroomMapper;
 
     @Override
-    public StrawResult sendRegisterCode(String phone,String inviteCode) {
+    @Transactional
+    public boolean sendRegisterCode(String phone, String inviteCode) {
 
         //判断邀请码是否正确,直接通过邀请码查找班级，如果班级不存在说明邀请码错误
         QueryWrapper query=new QueryWrapper();
         query.eq("invite_code",inviteCode);
         Classroom classroom = classroomMapper.selectOne(query);
         if(classroom==null){
-            return new StrawResult().failed("邀请码错误");
+            throw  new BusinessException("邀请码错误");
         }
-
-
         //先判断号码是否已被注册
         QueryWrapper queryWrapper=new QueryWrapper();
         queryWrapper.eq("username",phone);
         User user = userService.getOne(queryWrapper);
         if(user!=null){
-            return new StrawResult().failed("该手机号已注册！请直接登录！");
+            throw  new BusinessException("该手机号已注册！请直接登录！");
         }
 
-        return sendAliyunMesageIdentifyingCode(phone, RedisKeyPrefix.REGISTER_CODE_PREFIX,mesageConfig.getTemplateCode_register());
+      return  sendIdentifyingCode(phone, RedisKeyPrefix.REGISTER_CODE_PREFIX,mesageConfig.getTemplateCode_register(),4);
     }
 
     /**
@@ -85,28 +78,29 @@ public class MessageServiceImpl implements IMessageService {
      * @return
      */
     @Override
-    public StrawResult sendResetPasswordCode(String phone) {
+    @Transactional
+    public boolean sendResetPasswordCode(String phone) {
         QueryWrapper queryWrapper=new QueryWrapper();
         queryWrapper.eq("username",phone);
         User user = userService.getOne(queryWrapper);
         if(user==null){
-            return new StrawResult().failed("该手机号未注册，请先注册！");
+            throw  new BusinessException("该手机号未注册，请先注册！");
         }
-        return sendAliyunMesageIdentifyingCode(phone, RedisKeyPrefix.RESET_PASSWORD_CODE_PREFIX,mesageConfig.getTemplateCode_resetPassword());
+        return sendIdentifyingCode(phone, RedisKeyPrefix.RESET_PASSWORD_CODE_PREFIX,mesageConfig.getTemplateCode_resetPassword(),4);
     }
 
     /**
-     * 发送各种4位数的短信验证码
+     * 发送短信验证码
      * @param phone 电话号码
      * @param templateCode 短息模板
      * @param redisKeyPrefix redis的key的前缀
      * @return
      * @throws ClientException
      */
-    private StrawResult sendAliyunMesageIdentifyingCode(String phone,String redisKeyPrefix,String templateCode){
+    private boolean sendIdentifyingCode(String phone, String redisKeyPrefix, String templateCode,int size){
 
         try {
-            String verificationCode= NumberUtils.generateRandomNum(4);
+            String verificationCode= NumberUtils.generateRandomNum(size);
             DefaultProfile profile = DefaultProfile.getProfile(mesageConfig.getRegionId(), mesageConfig.getAccessKeyId(), mesageConfig.getSecret());
             IAcsClient client = new DefaultAcsClient(profile);
             CommonRequest request = new CommonRequest();
@@ -121,8 +115,28 @@ public class MessageServiceImpl implements IMessageService {
             request.putQueryParameter("TemplateParam", "{\"code\":\""+verificationCode+"\"}");
             CommonResponse response = client.getCommonResponse(request);
             //把验证码保存到redis,有效时间为5分钟
-            strRedisTemplate.opsForValue().set(redisKeyPrefix+phone,verificationCode,5, TimeUnit.MINUTES);
-            return getAliyunMessageStrawResult(phone, response);
+            String key=redisKeyPrefix+phone;
+            log.debug("====把验证码保存到redis,有效时间为5分钟====");
+            log.debug("存储的键为：{}，值为：{}",key,verificationCode);
+            strRedisTemplate.opsForValue().set(key,verificationCode,5, TimeUnit.MINUTES);
+            String data = response.getData();
+            JSONObject jsonObject = JSONObject.parseObject(data);
+            //短信发送状态
+            String code = jsonObject.getString("Code");
+            String message=jsonObject.getString("Message");
+            if("OK".equals(code)){
+                //短信如果发送成功，程序到此结束，不会继续往下执行
+                return true;
+            }else if("isv.MOBILE_NUMBER_ILLEGAL".equals(code)){
+                log.info(phone+"无效号码");
+               throw new BusinessException(phone+"无效号码");
+            }else if("isp.OUT_OF_SERVICE".equals(code)){
+                log.info(phone+"业务停机");
+                throw new BusinessException(phone+"业务停机");
+            }else{
+                log.info("短信发送失败，原因："+message);
+                throw new BusinessException("短信发送服务繁忙，请稍后再试！");
+            }
         }catch (ServerException e){
             log.error("ErrorCode=" + e.getErrCode());
             log.error("ErrorMessage=" + e.getErrMsg());
@@ -131,31 +145,10 @@ public class MessageServiceImpl implements IMessageService {
             log.error("ErrorCode=" + e.getErrCode());
             log.error("ErrorMessage=" + e.getErrMsg());
         }
-        return new StrawResult().failed("短信发送服务繁忙，请稍后再试！");
+        //运行到后面说明发送失败
+        log.info("短信发送失败！");
+        return false;
     }
 
-    /**
-     * 解析并封装返回发送短信的结果
-     * @param phone
-     * @param response
-     * @return
-     */
-    private StrawResult getAliyunMessageStrawResult(String phone, CommonResponse response) {
-        String data = response.getData();
-        System.out.println("data:"+data);
-        JSONObject jsonObject = JSONObject.parseObject(data);
-        //短信发送状态
-        String code = jsonObject.getString("Code");
-        String message=jsonObject.getString("Message");
-        if("OK".equals(code)){
-            return new StrawResult().success("发送成功!");
-        }else if("isv.MOBILE_NUMBER_ILLEGAL".equals(code)){
-            return new StrawResult().failed(phone+"无效号码");
-        }else if("isp.OUT_OF_SERVICE".equals(code)){
-            return new StrawResult().failed(phone+"业务停机");
-        }else{
-            log.error("短信发送失败，原因："+message);
-            return new StrawResult().failed("短信发送服务繁忙，请稍后再试！");
-        }
-    }
+
 }
