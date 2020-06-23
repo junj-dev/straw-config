05fcec9002c11bef6b9944b7e6c3dc482cba322c
haibao <1016005502@qq.com>
2020-06-23 11:21:11 +0800

    重构代码，StrawResult类重命名为R

--- straw-portal/src/main/java/cn/tedu/straw/portal/controller/NoticeController.java
+++ straw-portal/src/main/java/cn/tedu/straw/portal/controller/NoticeController.java
@@ -1,11 +1,9 @@
 package cn.tedu.straw.portal.controller;
 
 
-import cn.tedu.straw.common.StrawResult;
+import cn.tedu.straw.common.R;
 import cn.tedu.straw.portal.base.BaseController;
-import cn.tedu.straw.portal.mapper.QuestionMapper;
 import cn.tedu.straw.portal.model.Notice;
-import cn.tedu.straw.portal.model.Question;
 import cn.tedu.straw.portal.service.INoticeService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.github.pagehelper.PageInfo;
@@ -34,10 +32,10 @@ public class NoticeController extends BaseController {
     @GetMapping("/list")
     @ApiOperation("获取本用户的消息")
     @ResponseBody
-    public StrawResult selectMyNoticeList(){
+    public R selectMyNoticeList(){
 
        List<Notice> noticeList= noticeService.getMyNoticeList();
-       return new StrawResult().success(noticeList);
+       return R.success(noticeList);
     }
 
     @GetMapping("/all")
@@ -49,21 +47,21 @@ public class NoticeController extends BaseController {
     @PostMapping("/getAll")
     @ApiOperation("获取所有的消息")
     @ResponseBody
-    public StrawResult<PageInfo<Notice>> getAll(@RequestParam(value = "pageNum",defaultValue = "1") Integer pageNum, @RequestParam(value = "pageSize",defaultValue = "10")Integer pageSize){
+    public R<PageInfo<Notice>> getAll(@RequestParam(value = "pageNum",defaultValue = "1") Integer pageNum, @RequestParam(value = "pageSize",defaultValue = "10")Integer pageSize){
         PageInfo<Notice> pageInfo= noticeService.getAllNotice(pageNum,pageSize);
-      return new StrawResult<PageInfo<Notice>>().success(pageInfo);
+      return R.success(pageInfo);
     }
 
 
     @GetMapping("/count")
     @ApiOperation("获取本用户的消息")
     @ResponseBody
-    public StrawResult countNotice(){
+    public R countNotice(){
         QueryWrapper queryWrapper=new QueryWrapper();
         queryWrapper.eq("user_id",getUseId());
         queryWrapper.eq("read_status",false);
         int count = noticeService.count(queryWrapper);
-        return new StrawResult().success(count);
+        return R.success(count);
     }
 
     @GetMapping("/detail/{questionId}/{noticeId}")
